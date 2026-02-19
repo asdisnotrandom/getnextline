4bbc67fc41e58c54c664809134edcfd957838566
ademirel <ademirel@student.42istanbul.com.tr>
2026-02-19 04:36:54 +0300

    js

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ademirel <ademirel@student.42istanbul.com.tr>+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 22:47:52 by ademirel          #+#    #+#             */
-/*   Updated: 2026/02/19 03:50:48 by ademirel         ###   ########.fr       */
+/*   Updated: 2026/02/19 04:11:36 by ademirel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,13 @@ static char	*ft_clear(char *a, char **c)
 	return (NULL);
 }
 
-static char	*ft_nbal(char *s, int c)
+static char	*ft_nbal(char *s)
 {
 	char	*a;
 	char	*b;
 
 	if (s == NULL)
 		return (NULL);
-	if (c == 0)
-	{
-		free(s);
-		return (NULL);
-	}
 	b = ft_strchr(s, '\n');
 	if (b == NULL || *(b + 1) == '\0')
 	{
@@ -71,35 +66,44 @@ static char	*ft_lft(char *a)
 	return (b);
 }
 
+static char	*ft_read(int fd, char **blp, char *inc)
+{
+	int	c;
+
+	c = 1;
+	while (ft_strchr(*blp, '\n') == NULL && c != 0)
+	{
+		c = read(fd, inc, BUFFER_SIZE);
+		if (c == -1)
+			return (ft_clear(inc, blp));
+		inc[c] = '\0';
+		if (c == 0)
+			break ;
+		*blp = ft_strjoin(*blp, inc);
+		if (*blp == NULL)
+			return (ft_clear(inc, blp));
+	}
+	free(inc);
+	return (*blp);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*blp;
 	char		*inc;
 	char		*res;
-	int			c;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	inc = malloc(BUFFER_SIZE + 1);
 	if (inc == NULL)
 		return (ft_clear(NULL, &blp));
-	c = 1;
-	while (ft_strchr(blp, '\n') == NULL && c != 0)
-	{
-		c = read(fd, inc, BUFFER_SIZE);
-		if (c == -1)
-			return (ft_clear(inc, &blp));
-		inc[c] = '\0';
-		if (c == 0)
-			break ;
-		blp = ft_strjoin(blp, inc);
-		if (blp == NULL)
-			return (ft_clear(inc, &blp));
-	}
-	free(inc);
+	blp = ft_read(fd, &blp, inc);
+	if (blp == NULL)
+		return (NULL);
 	res = ft_lft(blp);
 	if (res == NULL)
 		return (ft_clear(NULL, &blp));
-	blp = ft_nbal(blp, c);
+	blp = ft_nbal(blp);
 	return (res);
 }
